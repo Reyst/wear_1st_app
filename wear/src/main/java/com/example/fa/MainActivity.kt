e00e9ca12c9940fa6b8f7aca65b22271b0844a5d
Ben <madben.87@gmail.com>
2020-02-12 15:43:43 +0200

    error can not allow permission

--- wear/src/main/java/com/example/fa/MainActivity.kt
+++ wear/src/main/java/com/example/fa/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.fa
 
+import android.content.Context
+import android.content.Intent
+import android.net.*
 import android.os.Bundle
+import android.provider.Settings
 import android.support.wearable.activity.WearableActivity
 import android.util.Log
 import android.widget.Toast
-import com.ben.shared.*
-import com.google.android.gms.wearable.*
+import com.ben.shared.CAPABILITY_PHONE_APP
+import com.ben.shared.MESSAGE_PATH_WEAR_CLEAR_COUNT
+import com.google.android.gms.wearable.CapabilityClient
+import com.google.android.gms.wearable.CapabilityInfo
+import com.google.android.gms.wearable.MessageClient
+import com.google.android.gms.wearable.Wearable
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : WearableActivity() {
 
     private val capabilityClient: CapabilityClient by lazy { Wearable.getCapabilityClient(this)}
     private val messageClient: MessageClient by lazy { Wearable.getMessageClient(this)}
+    private val connectivityManager: ConnectivityManager by lazy { getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +38,8 @@ class MainActivity : WearableActivity() {
         clear_btn.setOnClickListener {
             sendMessageToPhone(MESSAGE_PATH_WEAR_CLEAR_COUNT, "message from wearable".toByteArray())
         }
+
+        network()
     }
 
     private fun sendMessageToPhone(msg: String, data: ByteArray? = null) {
@@ -69,6 +81,38 @@ class MainActivity : WearableActivity() {
                 else -> "Fail: ${it.exception?.message}"
             }
             Toast.makeText(this@MainActivity, text, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun network() {
+        //val bandwidth: Int = connectivityManager.
+
+        val networkCallback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                Log.d("INSPECT", ":::::: Network onAvailable")
+            }
+
+            override fun onUnavailable() {
+                super.onUnavailable()
+                Log.d("INSPECT", ":::::: Network onUnavailable")
+            }
+        }
+
+        val request: NetworkRequest = NetworkRequest.Builder().run {
+            addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+            addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+            addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)
+            addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            build()
+        }
+
+        if (Settings.System.canWrite(this)) {
+            connectivityManager.requestNetwork(request, networkCallback)
+        } else {
+            val goToSettings = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
+            goToSettings.data = Uri.parse("package:" + this.packageName)
+            startActivity(goToSettings)
         }
     }
 
